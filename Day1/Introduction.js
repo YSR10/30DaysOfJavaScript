d8ed0db5b7da64912df84290f43ca75b62b38dbb
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-03-06 11:26:10 +0100

    wip

---
+++ Day1/Introduction.js
@@ -0,0 +1,35 @@
+//comments can make code readable
+
+//Welcome to30DaysOfJavaScript
+
+/*comments can make code readable
+ easy to reuse and informative*/
+
+ const Name ="yasir"
+ const wolrd ='the world is sphere'
+ let Welcome
+ let notting
+
+ console.log(typeof'name')
+ console.log(typeof 8)
+ console.log(typeof true )
+ console.log(typeof undefined )
+ console.log(typeof null)
+
+ let name
+ let book
+ let rule
+ let Well
+
+ const open ='close'
+ const front ='back'
+ const color ='black'
+ const yasir ='sulyman'
+
+ let firstName ='yasir'
+ let lastName ='sulyman'
+ let maritalStatus ='single'
+ let country ='nigeria'
+ let age ='19'
+
+ let ffirstName ='yasir', llastName ='sulyman',mmaritalStatus ='single', ccountry ='nigeria',aage ='19'
