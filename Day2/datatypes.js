5e542617eae2f5f449b1aa2f86fba4abc20708bd
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-03-08 14:21:30 +0100

    wip:day2 level 1

---
+++ Day2/datatypes.js
@@ -0,0 +1,29 @@
+//LEVEL 1
+
+let challenge ="30 Days Of JavaScript"
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(0, 2))
+console.log(challenge.substring(3, 21 ))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+const companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(companies.split(', '))
+console.log(challenge.replace("JavaScript", "Python"))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt('J'))
+console.log(challenge.indexOf('a'))
+console.log(challenge.lastIndexOf('a'))
+const sentence = "You cannot end a sentence with because because because is a conjunction"
+console.log(sentence.indexOf('because'))
+console.log(sentence.lastIndexOf('because'))
+console.log(sentence.search('because'))
+console.log(challenge.trim(''))
+console.log(challenge.startsWith('30 Days Of JavaScript'))
+console.log(challenge.endsWith('30 Days Of JavaScript'))
+console.log(challenge.match('a'))
+console.log(challenge.concat(''))
+console.log(challenge.repeat('2'))
