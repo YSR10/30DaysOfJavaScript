40fec2269fdeff7a6a069fdbc310470964e38d46
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2023-03-09 19:32:01 +0100

    wip:mathoperators

--- Day2/datatypes level 2.js
+++ Day2/datatypes level 2.js
@@ -3,3 +3,15 @@ console.log(quote)
 
 const teresa = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead"
 console.log(teresa)
+
+const type = '10'
+let of = type
+console.log(type == of)
+
+ let float = "9.8";
+ let num = (9.8);
+ console.log(Math.ceil(float));
+
+ let py = "python"
+ let ja = "jargon"
+ console.log(py.includes("on") && ja.includes("on") )
